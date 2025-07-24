4e6a67dcbf191419b1aa235835c0ede11f003115
cuongpv_mb <cuongpv@intern.vn>
2025-07-24 11:51:59 +0700

    Update

--- lib/core/presentation/app.dart
+++ lib/core/presentation/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_getx_clean_architecture/core/presentation/controllers/app_controller.dart';
-import 'package:flutter_getx_clean_architecture/core/presentation/navigation/navigation_src.dart';
 import 'package:flutter_getx_clean_architecture/core/presentation/widgets/base_get_page.dart';
 import 'package:flutter_getx_clean_architecture/generated/locales.g.dart';
+import 'package:flutter_getx_clean_architecture/routes/app_pages.dart';
+import 'package:flutter_getx_clean_architecture/routes/app_routes.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
--- lib/core/presentation/navigation/navigation_src.dart
+++ lib/core/presentation/navigation/navigation_src.dart
@@ -1,3 +1,1 @@
 export 'app_navigator.dart';
-export '../../../routes/app_routes.dart';
-export '../../../routes/app_pages.dart';
